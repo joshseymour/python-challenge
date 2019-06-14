2d402124e7a10bc69382e65d50a528744ac3f359
Josh Seymour <50425118+joshseymour@users.noreply.github.com>
2019-06-14 11:48:31 -0400

    Added additional comments to code

--- PyPoll.py
+++ PyPoll.py
@@ -5,14 +5,14 @@ import csv
 #set file path 
 csvpath = os.path.join("Resources", "election_data.csv")
 
-#create counter(s)
+#create counters
 votes = 0
 winning = 0
 #create list without headers
 data = []
 #create list of candidates
 candidates = []
-#create dictionary of candidate & vote count
+#create dictionaries of candidate votes  & percentage of votes
 candidate_vote = {}
 candidate_percent = {}
 
@@ -23,38 +23,38 @@ with open(csvpath, newline="") as csvfile:
 
         data = list(csvreader)
 
+#loop thorugh to count the votes and get candidate names
 for row in data:
         #count the number of votes
         votes = votes + 1
         #condition to append unique candidate and get count of votes
-        #Find count votes for each candiate
         if row[2] not in candidates:
                 candidates.append(row[2])
                 candidate_vote[row[2]] = 0
         candidate_vote[row[2]] = candidate_vote[row[2]] + 1
-#Find count of votes for each candiate / total votes (i.e. votes) * 100 = % of votes
+
+#Find count of votes for each candiate
 for (key, value) in candidate_vote.items():
     candidate_percent[key] = round(value/votes * 100, 1)
     if winning < value:
         winning = value
         elected_candidate = key
 
-
+#print out the results of the election
 print("Election Results")
 print("-------------------------")
 print("Total Votes: " + str(votes))
 print("-------------------------")
 for (key,value) in candidate_vote.items():
-        print(f'{key} {candidate_percent[key]}% ({value})')
+        print(f'{key}: {candidate_percent[key]}% ({value})')
 print("-------------------------")
 print("Winner: " + elected_candidate)
 print("-------------------------")
 
 # Specify the file to write to
-output_path = os.path.join("pypoll.txt")
+output_path = os.path.join("pypoll_output.txt")
 
-# Open the file using "write" mode. Specify the variable to hold the contents
-# can also delcare write as the mode, with open(output_path, mode="w', newline=' ') as csvfile:
+# Output results to .txt file in same directory
 with open(output_path, 'w', newline='') as csvfile:
 
     # Initialize csv.writer
@@ -65,7 +65,7 @@ with open(output_path, 'w', newline='') as csvfile:
     csvwriter.writerow(['Total Votes: ' + str(votes)])
     csvwriter.writerow(['-------------------------'])
     for (key,value) in candidate_vote.items():
-        csvwriter.writerow([f'{key} {candidate_percent[key]}% ({value})'])
+        csvwriter.writerow([f'{key}: {candidate_percent[key]}% ({value})'])
     csvwriter.writerow(['-------------------------'])
     csvwriter.writerow(['Winner: ' + elected_candidate])
     csvwriter.writerow(['-------------------------'])
